86ac508b6b8e0a8c37afeb3d009d6d271f5b0734
Riviets <123061245+Riviets@users.noreply.github.com>
2023-10-28 16:50:30 +0300

    Add files via upload
    BFS has been added

--- algorithms/main.js
+++ algorithms/main.js
@@ -28,8 +28,52 @@ function quickSort(arr) { //O(n * log(n))
     }
     return [...quickSort(less), tmp, ...quickSort(more)]
 }
-let arr = [9, 7, 27, 15, 42, 2]
-let sortedArr = quickSort(arr)
-console.log(arr)
-console.log(sortedArr)
-console.log(binarySearch(sortedArr, 15))
+let graph = {
+    "1": ["2","3"],
+    "2": ["1","3","5(s)"],
+    "3": ["1","2","4","5(s)"],
+    "4": ["3"],
+    "5(s)": ["2","3"]
+}
+let graph2 = {
+    "1": ["2(s)","3"],
+    "2(s)": ["1","3","5(s)"],
+    "3": ["1","2(s)","4","5(s)"],
+    "4": ["3"],
+    "5(s)": ["2(s)","3"]
+}
+
+function isSearched(name) {
+    return name.slice(-3) === "(s)"
+}
+let queue = []
+let visited = []
+function bfs(graph, start) {
+    queue.push(start)
+    while (queue.length > 0) {
+        let current = queue.shift()
+        if (!visited.includes(current)) {
+            if (isSearched(current)) {
+                return current
+            }
+            else {
+                let neighbors = graph[current]
+                for (let i = 0; i < neighbors.length; i++) {
+                    queue.push(neighbors[i])
+                }
+            }
+        }
+        visited.push(current)
+    }
+    return null
+}
+console.log(bfs(graph, "1"))
+visited = []
+queue = []
+console.log(bfs(graph2, "1"))
+
+// let arr = [9, 7, 27, 15, 42, 2]
+// let sortedArr = quickSort(arr)
+// console.log(arr)
+// console.log(sortedArr)
+// console.log(binarySearch(sortedArr, 15))
